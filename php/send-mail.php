08221dcf498a77d53a63b40d23b862877edb6bf0
François Dancre <francois.dcre@gmail.com>
2025-03-25 23:22:28 +0100

    Adding Recaptcha

--- php/send-mail.php
+++ php/send-mail.php
@@ -7,6 +7,38 @@ require '../assets/lib/PHPMailer-6.9.3/src/Exception.php';
 require '../assets/lib/PHPMailer-6.9.3/src/PHPMailer.php';
 require '../assets/lib/PHPMailer-6.9.3/src/SMTP.php';
 
+// Récupération des variables d'environnement
+$env = parse_ini_file(__DIR__ . '/../../.env');
+$recaptchaSecret = $env['RECAPTCHA_SECRET'] ?? null;
+$password = $env['EMAIL_PASSWORD'] ?? null;
+
+// Vérification de reCAPTCHA
+$recaptchaResponse = $_POST['g-recaptcha-response'] ?? '';
+$remoteIp = $_SERVER['REMOTE_ADDR'];
+
+$verifyUrl = 'https://www.google.com/recaptcha/api/siteverify';
+$data = [
+    'secret'   => $recaptchaSecret,
+    'response' => $recaptchaResponse,
+    'remoteip' => $remoteIp
+];
+
+$options = [
+    'http' => [
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    ]
+];
+
+$context  = stream_context_create($options);
+$result = file_get_contents($verifyUrl, false, $context);
+$responseData = json_decode($result);
+
+if (!$responseData || !$responseData->success) {
+    echo "La vérification reCAPTCHA a échoué. Veuillez réessayer.";
+    exit;
+}
 
 $mail = new PHPMailer(true);
 
@@ -16,10 +48,6 @@ try {
     $mail->Host       = 'francois-dancre.fr';   // Spécifiez le serveur SMTP
     $mail->SMTPAuth   = true;                                   
     $mail->Username   = 'formulaire@francois-dancre.fr';  // Votre adresse e-mail
-
-    $env = parse_ini_file(__DIR__ . '/../../.env');
-    $password = $env['EMAIL_PASSWORD'] ?? null;
-
     $mail->Password   = $password;           // Votre mot de passe
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;    // Ou PHPMailer::ENCRYPTION_STARTTLS selon les exigences
     $mail->Port       = 465;                            // Ou 587 si vous utilisez TLS
